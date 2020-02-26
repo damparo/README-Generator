8de89354ea847f6c286bcd2f1ead98064ea8f3f2
Dennis Amparo <dennisamparo11@gmail.com>
2020-02-26 10:00:27 -0700

    setup

---
+++ api.js
@@ -0,0 +1,11 @@
+const api = {
+    getUser(username) {
+  
+    }
+  };
+  
+  module.exports = api;
+
+//   ${username}?client_id=${
+//     process.env.CLIENT_ID
+//     }&client_secret=${process.env.CLIENT_SECRET}`
