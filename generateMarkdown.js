d0149fc7e27e8724b0f207363ef082763e81fefb
Dennis Amparo <dennisamparo11@gmail.com>
2020-02-28 20:26:02 -0700

    prefinal

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,8 +1,42 @@
 function generateMarkdown(data) {
     return `
-  # ${data.title}
-  
-  `;
+    # ${data.name}
+    ![github-med](https://avatars1.githubusercontent.com/u/59849741?s=400&v=4)
+     
+    ![GitHub license](https://img.shields.io/badge/license-${data.license}-blue.svg)
+
+    ## Project Name: 
+     ${data.projectname}
+    ## Table of Contents:
+    - [Description](#Description)
+    - [Install-Dependencies]       (#Install-Dependencies)
+    - [Test](#Test)
+    - [Repository](#Repository)
+    - [Contributing](#Contributing)
+    - [Followers](#Followers)
+    - [Contact](#Contact)
+    - [Questions](#Questions)
+    
+    ## Description: 
+     ${data.description}
+    ## Install Dependencies: 
+    \`${data.dependencies}\`
+    ## Test: 
+    \`${data.test}\`
+    ## Repository: 
+     ${data.repo}
+    ## Contributing: 
+     ${data.contributing}
+    ## Contact: 
+     ${data.email || "dennisamparo11@gmail.com"}
+    
+     ## [Link to deployed app]:(https://damparo.github.io/README-Generator/)
+    
+    ## Questions
+    Contact me for further questions, thanks!
+    `;
+
+
   }
   
   module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,14 +1,122 @@
 const inquirer = require("inquirer");
-
-
-console.log(apiGit);
-
+const fs = require("fs");
+const api = require("./api.js")
+const generateMarkdown = require("./generateMarkdown.js")
 
 inquirer
-  .prompt({
-    message: "Enter your GitHub username",
-    name: "username"
-  })
+  .prompt(
+    [
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+      },
+      {
+        type: "input",
+        name: "projectname",
+        message: "What is your project's name?"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "please write a short description of your project?"
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "What Kind of license should your project have?",
+        choices: ["MIT", "Apache 2.0", "Gp/3.0", "BSD3", "None"]
+      },
+      {
+        type: "input",
+        name: "dependencies",
+        message: "What command should be run to install dependencies?"
+      },
+      {
+        type: "input",
+        name: "test",
+        message: "What command should be run to run tests?"
+      },
+      {
+        type: "input",
+        name: "repo",
+        message: "What does the user need to know about using the repo?"
+      },
+      {
+        type: "input",
+        name: "contributing",
+        message: "What does the user need to know about contributing to the repo?"
+      },
+      {
+        type: "input",
+        name: "username",
+        message: "Enter your GitHub username",
+        
+      }
+    ])
+  // 
+  .then(function(data) {
+    api.getUser(data.username).then(({
+      data }) => {
+      console.log(data);
+      
+
+
+      var filename = "README.md";
+
+      fs.writeFileSync(filename, generateMarkdown({...data}), function(err) {
+        if (err) {
+          return console.log(err);
+        }
+
+        console.log("Success!");
+      });
+    });
+  });
+
+// const questions = [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // const questions = [ 
